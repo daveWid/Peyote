183e84eafe378673fa8f7ee5ea600ee4bb70a7dd
Dave Widmer <dwidmer@bgsu.edu>
2012-03-14 09:36:03 -0400

    Adding a generic exception class.

---
+++ classes/Peyote/Exception.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Peyote;
+
+/**
+ * A Peyote Exception class
+ *
+ * @package    Peyote
+ * @author     Dave Widmer <dave@davewidmer.net>
+ */
+class Exception extends \Exception{}
