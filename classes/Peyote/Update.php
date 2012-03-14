a28c0c8792b14c5e1fc4863c5ba50ae7d6254561
Dave Widmer <dwidmer@bgsu.edu>
2012-03-14 14:05:51 -0400

    Initial Update class ready.

---
+++ classes/Peyote/Update.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Peyote;
+
+/**
+ * A class that builds UPDATE statments.
+ *
+ * @link       http://dev.mysql.com/doc/refman/5.0/en/update.html
+ *
+ * @package    Peyote
+ * @author     Dave Widmer <dave@davewidmer.net>
+ */
+class Update extends \Peyote\Base
+{
+	/**
+	 * @var array  The data to update
+	 */
+	private $data = array();
+
+	/**
+	 * @var \Peyote\Where  The where clause
+	 */
+	protected $where;
+
+	/**
+	 * @var \Peyote\OrderBy  The order by clause
+	 */	
+	protected $order_by;
+
+	/**
+	 * @var \Peyote\Limit  The limit clause
+	 */
+	protected $limit;
+
+	/**
+	 * Create a new Update instance.
+	 *
+	 * @param mixed $table   The table name OR array($table, $alias)
+	 */
+	public function __construct($table = "", \Peyote\Where $where = null)
+	{
+		$this->table($table);
+		$this->where = ($where !== null) ? $where : new \Peyote\Where;
+		$this->order_by = new \Peyote\OrderBy;
+		$this->limit = new \Peyote\Limit;
+	}
+
+	/**
+	 * Sets the values as column => value pairs
+	 *
+	 * @param  array $data  An array of column => value pairs
+	 * @return $this
+	 */
+	public function set(array $data)
+	{
+		$this->data = array_merge($this->data, $data);
+		return $this;
+	}
+
+	/**
+	 * Compiles the query into raw SQL
+	 *
+	 * @return  string
+	 */
+	public function compile()
+	{
+		$sql = array("UPDATE");
+		$sql[] = $this->table();
+		$sql[] = "SET";
+
+		$data = array();
+		foreach ($this->data as $column => $value)
+		{
+			$data[] = "{$column} = {$value}";
+		}
+
+		$sql[] = implode(", ", $data);
+
+		// Where?
+		$where = $this->where->compile();
+		if ($where !== "")
+		{
+			$sql[] = $where;
+		}
+
+		// Order?
+		$order = $this->order_by->compile();
+		if ($order !== "")
+		{
+			$sql[] = $order;
+		}
+
+		// Limit?
+		$limit = $this->limit->compile();
+		if ($limit !== "")
+		{
+			$sql[] = $limit;
+		}
+
+		return implode(" ", $sql);
+	}
+
+	/**
+	 * Get the class properties to use as "traits".
+	 *
+	 * @return array
+	 */
+	protected function traits()
+	{
+		return array("where","order_by","limit");
+	}
+
+}
---
+++ tests/UpdateTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * A class for testing the Update class.
+ *
+ * @package    Peyote
+ * @category   Tests
+ * @author     Dave Widmer <dave@davewidmer.net>
+ */
+class UpdateTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @var array  Data we can use for update testing
+	 */
+	protected $hero = array(
+		'name' => "Frank Castle",
+		'alias' => "Punisher"
+	);
+
+	/**
+	 * Testing a very simple update statement
+	 */
+	public function testSimple()
+	{
+		$update = new \Peyote\Update;
+		$update->table("heroes")->set($this->hero);
+
+		$this->assertEquals("UPDATE heroes SET name = Frank Castle, alias = Punisher", $update->compile());
+	}
+
+}
