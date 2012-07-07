630226b0183db98df3060feaf88c38622a650678
Dave Widmer <dave@davewidmer.net>
2012-07-07 17:52:08 -0400

    Using Composer autoloading for the unit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,14 +1,3 @@
 <?php
 
-// Load up clases needed for the tests
-foreach (glob("tests/classes/*.php") as $filename)
-{
-    include $filename;
-}
-
-// Autoloading
-$base = realpath(dirname(__FILE__).DIRECTORY_SEPARATOR."..").DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR;
-
-$loader = new SplClassLoader;
-$loader->setIncludePath($base);
-$loader->register();
+include dirname(__DIR__).DIRECTORY_SEPARATOR."vendor".DIRECTORY_SEPARATOR."autoload.php";
