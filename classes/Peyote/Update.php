47b9e1319f1bcb759a5f0e47d7afcb2a629aaca5
Dave Widmer <dave@davewidmer.net>
2012-07-08 17:25:23 -0400

    Finishing up the Update query which finishes things up.

---
+++ classes/Peyote/Update.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Peyote;
+
+/**
+ * Building a UPDATE query.
+ *
+ * See Select for the full rant, but the tl;dr is that php 4 is messing up my
+ * classes, so I have to actually declare where() and limit() in this class
+ * instead of just using the cool passthru functionality.
+ *
+ * @package    Peyote
+ * @author     Dave Widmer <dave@davewidmer.net>
+ */
+class Update extends \Peyote\Query
+{
+	/**
+	 * @var \Peyote\Where  The where object
+	 */
+	protected $where = null;
+
+	/**
+	 * @var \Peyote\Order  The order object
+	 */
+	protected $order_by = null;
+
+	/**
+	 * @var \Peyote\Limit  The limit object
+	 */
+	protected $limit = null;
+
+	/**
+	 * @var array  The key/value pair data
+	 */
+	private $data = array();
+
+	/**
+	 * @var array  A list of traits that the query can passthru
+	 */
+	protected $traits = array('where', 'order_by', 'limit');
+
+	/**
+	 * Optionally sets the table name and initializes the internal class
+	 * properties.
+	 *
+	 * @param string $table  The name of the table
+	 */
+	public function __construct($table = null)
+	{
+		$this->where = new \Peyote\Where;
+		$this->order_by = new \Peyote\Order;
+		$this->limit = new \Peyote\Limit;
+
+		parent::__construct($table);
+	}
+
+	/**
+	 * Set key/value pairs for the update.
+	 *
+	 * @param  array $data  The data to set
+	 * @return \Peyote\Update
+	 */
+	public function set(array $data)
+	{
+		$this->data = $data;
+		return $this;
+	}
+
+	/**
+	 * Adds a WHERE clause.
+	 *
+	 * @param  string $column  The column name
+	 * @param  string $op      The comparison operator
+	 * @param  mixed  $value   The value to bind
+	 * @return \Peyote\Delete
+	 */
+	public function where($column, $op, $value)
+	{	
+		$this->where->andWhere($column, $op, $value);
+		return $this;
+	}
+
+	/**
+	 * Sets the limit.
+	 *
+	 * @param  int $num  The number to limit the queries to
+	 * @return \Peyote\Select
+	 */
+	public function limit($num)
+	{
+		$this->limit->setLimit($num);
+		return $this;
+	}
+
+	/**
+	 * Gets all of the bound parameters for this query.
+	 *
+	 * @return array
+	 */
+	public function getParams()
+	{
+		/**
+		 * Instead of writing some crazy magic foo to pull the params
+		 * automatically, I'm just overridding this function to get the
+		 * correct params.
+		 */
+		return array_merge($this->params, $this->where->getParams());
+	}
+
+	/**
+	 * Compiles the query into raw SQL
+	 *
+	 * @return  string
+	 */
+	public function compile()
+	{
+		$sql = array("UPDATE");
+		$sql[] = $this->table;
+		$sql[] = "SET";
+
+		$set = array();
+		foreach ($this->data as $key => $value)
+		{
+			$set[] = "{$key} = ?";
+			$this->params[] = $value;
+		}
+
+		$sql[] = join(', ', $set);
+
+		foreach ($this->traits as $trait)
+		{
+			$compiled = $this->{$trait}->compile();
+			if ($compiled !== "")
+			{
+				$sql[] = $compiled;
+			}
+		}
+
+		return join(' ', $sql);
+	}
+
+}
---
+++ tests/UpdateTest.php
@@ -0,0 +1,50 @@
+<?php
+
+class UpdateTest extends PHPUnit_Framework_TestCase
+{
+	public $query;
+
+	public $data = array(
+		'name' => "Testy McTesterson"
+	);
+	
+	public function setUp()
+	{
+		parent::setUp();
+		$this->query = new \Peyote\Update('user');
+		$this->query->set($this->data);
+	}
+
+	public function testAll()
+	{
+		$sql = "UPDATE user SET name = ?";
+		$this->assertSame($sql, $this->query->compile());
+		$this->assertSame(array_values($this->data), $this->query->getParams());
+	}
+
+	public function testWhere()
+	{
+		$this->query->where('user_id', '=', 1);
+
+		$sql = "UPDATE user SET name = ? WHERE user_id = ?";
+		$this->assertSame($sql, $this->query->compile());
+		$this->assertSame(array('Testy McTesterson', 1), $this->query->getParams());
+	}
+
+	public function testOrderBy()
+	{
+		$this->query->orderBy('create_date', 'ASC');
+
+		$sql = "UPDATE user SET name = ? ORDER BY create_date ASC";
+		$this->assertSame($sql, $this->query->compile());
+	}
+
+	public function testLimit()
+	{
+		$this->query->limit(2);
+
+		$sql = "UPDATE user SET name = ? LIMIT 2";
+		$this->assertSame($sql, $this->query->compile());
+	}
+
+}
